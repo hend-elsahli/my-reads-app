b85c6fef20167ac8c0c787fb87632a04340197d1
Hend El-Sahli <hend.hassan.elsahli@gmail.com>
2021-08-14 14:29:03 +0200

    feat: Add books api functions

---
+++ src/api/books-api.js
@@ -0,0 +1,74 @@
+import { appFetch, PUT, getDefaultHeaders, POST } from ".";
+
+const getBooklist = (list) => {
+  return list.map(
+    ({ id, title, authors, shelf, imageLinks: { thumbnail } }) => ({
+      id,
+      title,
+      authors,
+      shelf,
+      thumbnailUrl: thumbnail,
+    })
+  );
+};
+/*---------------------------------
+          API-Functions
+---------------------------------*/
+export const getBook = async ({ id }) => {
+  const { ok, data } = await appFetch({
+    url: `books/${id}`,
+  });
+
+  if (!ok) return { ok: false, data: null };
+
+  if (ok) {
+    const {
+      id,
+      title,
+      authors,
+      shelf,
+      imageLinks: { thumbnail },
+    } = data.book;
+
+    return {
+      ok: true,
+      data: {
+        id,
+        title,
+        authors,
+        shelf,
+        thumbnailUrl: thumbnail,
+      },
+    };
+  }
+};
+
+export const getAllBooks = async () => {
+  const { ok, data } = await appFetch({
+    url: "books",
+  });
+
+  return ok ? { ok: true, data: getBooklist(data.books) } : { ok, data };
+};
+
+export const updateBook = async ({ id, shelf }) => {
+  const { ok } = await appFetch({
+    url: `books/${id}`,
+    method: PUT,
+    headers: { ...getDefaultHeaders(), "Content-Type": "application/json" },
+    body: { shelf },
+  });
+
+  return { ok };
+};
+
+export const searchBooks = async ({ query }) => {
+  const { ok, data } = await appFetch({
+    url: `search`,
+    method: POST,
+    headers: { ...getDefaultHeaders(), "Content-Type": "application/json" },
+    body: { query },
+  });
+
+  return ok ? { ok: true, data: getBooklist(data.books) } : { ok, data };
+};
---
+++ src/api/index.js
@@ -0,0 +1,69 @@
+/*---------------------------------
+            CONSTANTS
+---------------------------------*/
+export const BASE_URL = "https://reactnd-books-api.udacity.com";
+/** Status */
+export const OK = "ok";
+export const ERROR = "error";
+/** Methods */
+export const GET = "GET";
+export const POST = "POST";
+export const PUT = "PUT";
+export const PATCH = "PATCH";
+/** Errors */
+export const ERR_UNKNOWN = "unknown";
+
+/*---------------------------------
+            Helpers
+---------------------------------*/
+export const getDefaultHeaders = () => {
+  let token = localStorage.token;
+  if (!token)
+    token = localStorage.token = Math.random().toString(36).substr(-8);
+
+  return {
+    Accept: "application/json",
+    Authorization: token,
+  };
+};
+
+const handleResponseError = (res) => {
+  const { ok } = res;
+  if (!ok) {
+    throw new Error();
+  }
+
+  return res;
+};
+
+const handleApiError = (error) => {
+  console.log("handleApiError", error);
+  return { ok: false, error: ERR_UNKNOWN };
+};
+
+/**
+ *
+ * @returns Object { ok: true|false, data: []:{}:string, error: error-code-string }
+ * @Example { ok: false, error: ERR_UNKOWN }
+ * @example { ok: true, data: [{...}, {...}] }
+ */
+export const appFetch = async ({
+  url,
+  method,
+  body,
+  headers = getDefaultHeaders(),
+}) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${url}`, {
+      method,
+      body: body ? JSON.stringify(body) : body,
+      headers,
+    });
+
+    const data = await handleResponseError(res).json();
+
+    return { ok: true, data };
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
