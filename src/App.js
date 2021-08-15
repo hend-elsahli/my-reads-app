f01472fcc2b8055ed572205e6d3809de4cd98649
Hend El-Sahli <hend.hassan.elsahli@gmail.com>
2021-08-15 09:58:01 +0200

    feat: Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
 import Home from "./ui/pages/Home";
+import Search from "./ui/pages/Search";
 
 /** TODO: Make app responsive */
 function App() {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
+
+  // return (
+  //   <div className="app">
+  //     <Home />
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/ui/pages/Search/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/*---------------------------------
+            Component
+---------------------------------*/
+function Search() {
+  /** Render */
+  return (
+    <div>
+      <h1>Search...</h1>
+    </div>
+  );
+  /** Render */
+}
+
+export default Search;
